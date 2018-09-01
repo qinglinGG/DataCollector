eff75fb096327d239d3f444177842e72cbda3d6c
Administrator <289896071@qq.com>
2018-09-02 01:17:36 +0800

    index modules commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@
 //获取应用实例
 const app = getApp()
 
-Page({
+var pageObj = {
   data: {
     motto: 'Hello World',
     userInfo: {},
@@ -10,7 +10,8 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
+    console.log('onclick event');
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,7 +22,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -39,16 +40,55 @@ Page({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
+          
         }
       })
     }
+    var win = this;
+    console.log('What is po:', this);
+    this.loadingHis({
+      succ:function(hisList){
+        win.setData({
+          hisList: hisList
+      })
+      }
+    })
+
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  showHisList(hisList){
+    pageObj.setData({
+      hisList: hisList
+    });
+  },
+  loadingHis( cb ) {    
+    wx.request({
+      url: 'http://f.apiplus.net/qxc-20.json',
+      success: function (r) {
+        console.log("loadingHis succ", r);
+        try {
+          cb.succ(r.data.data);
+          //pageObj.showHisList(r.data.data);
+        } catch (net_excep) {
+          console.error('Query history fail, error_info is=', net_excep);
+        }
+        
+      },
+      complete: function (info) {
+        console.log('loadingHis complete');
+      },
+      fail: function (err) {
+        console.error('loadingHis fail', err);
+      }
+    })
   }
-})
+}
+
+Page( pageObj )
